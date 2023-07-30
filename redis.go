f2fd13510b25f90dcc788f978bd89d2deef524ea
Hyeonki Hong <hhk7734@gmail.com>
2023-07-30 22:16:22 +0900

    feat: use ulid for redis sorted set member

--- redis.go
+++ redis.go
@@ -5,6 +5,7 @@ import (
 	_ "embed"
 	"time"
 
+	"github.com/oklog/ulid/v2"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -30,7 +31,11 @@ var slidingWindowLogLuaScript *redis.Script
 
 func (r *RedisRateLimit) SlidingWindowLog(ctx context.Context, key string, limit int,
 	window time.Duration) (int, time.Duration, error) {
-	ret, err := slidingWindowLogLuaScript.Run(ctx, r.client, []string{key}, limit, window.Milliseconds(), time.Now().UnixMilli()).Int64Slice()
+	ret, err := slidingWindowLogLuaScript.Run(ctx, r.client, []string{key},
+		limit,
+		window.Milliseconds(),
+		time.Now().UnixMilli(),
+		ulid.Make().String()).Int64Slice()
 	if err != nil {
 		return 0, 0, err
 	}
--- slidingwindolog.lua
+++ slidingwindolog.lua
@@ -2,23 +2,24 @@ local key = KEYS[1]
 local limit = tonumber(ARGV[1])
 local window = tonumber(ARGV[2])
 local now = tonumber(ARGV[3])
+local member = ARGV[4]
 
 redis.call('ZREMRANGEBYSCORE', key, '-inf', now - window)
 
 local prevCount = redis.call('ZCARD', key)
 
-local first = redis.call('ZRANGE', key, 0, 0)
+local first = redis.call('ZRANGE', key, 0, 0, 'WITHSCORES')
 local reset
 if next(first) == nil then
     reset = window
 else
-    reset = window - (now - tonumber(first[1]))
+    reset = window - (now - tonumber(first[2]))
 end
 
 if prevCount >= limit then
     return { -1, reset }
 end
 
-redis.call('ZADD', key, now, now)
+redis.call('ZADD', key, now, member)
 redis.call('EXPIRE', key, window / 1000)
 return { limit - prevCount - 1, reset }
